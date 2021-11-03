596bc8450a5a479cb45c0b5f4588546a23b9157e
Ümmü Gülsüm <u.gulsumkarabudak@gmail.com>
2021-11-03 11:59:40 +0300

    Setting up Context API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify  = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState();
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -15,13 +17,41 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
-      spotify.setAccessToken(_token);
-    }
 
-    console.log("token", token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      console.log("[token]", token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
+      spotify.getPlaylist("37i9dQZF1E34Ucml4HHx1w").then((playlist) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: playlist,
+        });
+      });
+
+     
+    }
+    
   }, []);
-  return <div className="App"> {token ? <h1>Logged in</h1> : <Login />} </div>;
+
+  
+  return <div className="App"> {token ? <Player spotify={spotify} /> : <Login />} </div>;
 }
 
 export default App;
